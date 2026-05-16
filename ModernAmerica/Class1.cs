e46806f5c5b9a2aceddcf296bb3a22dc83a87783
AwesomeMeeps <151784194+only-meeps@users.noreply.github.com>
2026-05-16 11:10:02 -0700

    Fixed map spawning, made small changes to maps
    Changes:
    -Fixed player spawns on maps
    -Fixed bomb not spawning on one of the old maps
    -Changed the permanent towers on the large tower map to destructible ones
    -Added more bombs on old maps
    -Fixed textures not spawning for client

--- ModernAmerica/Class1.cs
+++ ModernAmerica/Class1.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
+using ExitGames.Client.Photon;
 using HarmonyLib;
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,6 +16,9 @@ public class ModernAmerica : BaseUnityPlugin
     {
         var harmony = new Harmony("com.meeps.ModernAmerica");
         harmony.PatchAll();
+        MapLoadPatch networkListener = new MapLoadPatch();
+        PhotonNetwork.AddCallbackTarget(networkListener);
+        ModVersion.version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
     }
     void Update()
     {
@@ -27,12 +32,29 @@ public class ModernAmerica : BaseUnityPlugin
     }
 }
 
+public static class ModVersion
+{
+    public static string version;
+}
+
+[HarmonyPatch(typeof(GM_Test), "GetSpawnPoint")]
+class PatchSpawn
+{
+    static bool Prefix(GM_Test __instance, ref Transform __result, int teamID)
+    {
+        __result = Object.FindObjectsOfType<MapSpawnPosition>()[teamID].transform;
+        Debug.Log("Spawn points in map: " + Object.FindObjectsOfType<MapSpawnPosition>().Length);
+        return false;
+    }
+}
+
 [HarmonyPatch(typeof(MapManager), "Start")]
 class InjectOldMaps
 {
     static bool Prefix(MapManager __instance)
     {
         Debug.Log("Starting MapManager...");
+
         MapManager.instance = __instance;
         List<GameObject> currentMaps = new List<GameObject>(__instance.maps);
         List<GameObject> hiddenMap = new List<GameObject>();
@@ -74,10 +96,110 @@ class InjectOldMaps
             maps[i].transform.parent = null;
             maps[i].transform.position = Vector3.zero;
             maps[i].transform.rotation = Quaternion.identity;
-            if (maps[i].GetComponent<PhotonView>() == null)
+
+
+            GameObject gameObject = maps[i];
+            if (gameObject.name.ToLower().Contains("oldmap"))
             {
-                PhotonView pv = maps[i].AddComponent<PhotonView>();
-                pv.ViewID = 0;
+                Debug.Log("Scaling up old map!");
+
+                if (gameObject.GetComponent<PhotonView>() == null)
+                {
+                    PhotonView pv = gameObject.AddComponent<PhotonView>();
+                    //PhotonTransformView view = gameObject.AddComponent<PhotonTransformView>();
+                    pv.ViewID = 0;
+                    //pv.ObservedComponents.Add(view);
+                }
+
+
+                gameObject.transform.localScale = new Vector3(100, 100, 100);
+
+
+                Debug.Log("Extracting textures from retail maps...");
+                List<Material> defaultMats = new List<Material>();
+                List<Transform> children = __instance.maps[0].GetComponentsInChildren<Transform>().ToList();
+                children.AddRange(__instance.maps[1].GetComponentsInChildren<Transform>().ToList());
+                children.AddRange(__instance.maps[2].GetComponentsInChildren<Transform>().ToList());
+                children.AddRange(__instance.maps[3].GetComponentsInChildren<Transform>().ToList());
+                children.AddRange(__instance.maps[4].GetComponentsInChildren<Transform>().ToList());
+                children.AddRange(__instance.maps[5].GetComponentsInChildren<Transform>().ToList());
+
+                foreach (Transform child in children)
+                {
+                    if (child.GetComponent<MeshRenderer>() != null)
+                    {
+                        if (defaultMats.FindIndex(x => child.GetComponent<MeshRenderer>().sharedMaterial.name.Contains(x.name)) == -1)
+                        {
+                            defaultMats.Add(child.GetComponent<MeshRenderer>().sharedMaterial);
+                        }
+                    }
+                }
+                Debug.Log("Textures extracted! List is as follows...");
+                foreach (Material mat in defaultMats)
+                {
+                    Debug.Log(mat.name);
+                }
+                Debug.Log("Loading textures...");
+                foreach (Transform child in gameObject.GetComponentsInChildren<Transform>(true))
+                {
+                    if (child.GetComponent<MeshRenderer>() != null)
+                    {
+                        int findIdx = defaultMats.FindIndex(x => child.GetComponent<MeshRenderer>().sharedMaterial.name.Contains(x.name));
+                        if (findIdx != -1)
+                        {
+                            //child.GetComponent<MeshRenderer>().sharedMaterial = defaultMats[findIdx];
+                            Material stolenMat = defaultMats[findIdx];
+                            Material instanceMat = new Material(stolenMat);
+                            instanceMat.DisableKeyword("_WORLDPOS_ON");
+                            instanceMat.DisableKeyword("_TRIPLANAR_ON");
+                            instanceMat.SetFloat("_WorldSpaceMapping", 0f);
+
+                            child.GetComponent<MeshRenderer>().material = instanceMat;
+                        }
+                    }
+                    Debug.Log($"Object {child.gameObject.name} : Active: {child.gameObject.activeSelf} : Map : {i}");
+                    if (child.name.ToLower().Contains("spawn"))
+                    {
+                        if (child.GetComponent<MapSpawnPosition>() != null)
+                        {
+                            UnityEngine.Object.Destroy(child.GetComponent<MapSpawnPosition>());
+                        }
+                        child.gameObject.AddComponent<MapSpawnPosition>();
+                        Debug.Log("Fixed spawn");
+                    }
+                }
+                foreach (MeshCollider cl in gameObject.GetComponentsInChildren<MeshCollider>())
+                {
+                    GameObject current = cl.gameObject;
+                    UnityEngine.Object.Destroy(cl);
+                    current.AddComponent<BoxCollider>();
+                }
+                Physics.SyncTransforms();
+                foreach (Rigidbody rb in gameObject.GetComponentsInChildren<Rigidbody>())
+                {
+                    rb.solverIterations = 20;
+                    rb.solverVelocityIterations = 20;
+                    rb.mass = 700;
+                    rb.ResetCenterOfMass();
+                    rb.ResetInertiaTensor();
+                    rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+                }
+                Physics.SyncTransforms();
+
+                foreach (Transform child in gameObject.GetComponentsInChildren<Transform>(true))
+                {
+                    GameObject current = child.gameObject;
+                    Rigidbody rb = current.GetComponent<Rigidbody>();
+
+                    if (rb != null)
+                    {
+                        Effectable effectable = current.AddComponent<Effectable>();
+                        Effectable_MapObject effectableMap = current.AddComponent<Effectable_MapObject>();
+
+
+                    }
+                }
+
             }
             Utils.RegisterPrefabWithPhoton(maps[i], "Maps");
         }
@@ -123,12 +245,20 @@ class PositionDebugger
     }
 }
 
+
 [HarmonyPatch(typeof(MapManager), "LoadRandomLevel")]
-class MapLoadPatch
+class MapLoadPatch : IOnEventCallback
 {
+    private const byte MAP_MOVE_EVENT_CODE = 99;
 
     static bool Prefix(MapManager __instance)
     {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            if (__instance.currentMap) PhotonNetwork.Destroy(__instance.currentMap);
+            return false;
+        }
+
         if (__instance.currentMap)
         {
             PhotonNetwork.Destroy(__instance.currentMap);
@@ -136,115 +266,51 @@ class MapLoadPatch
 
         int num = Random.Range(0, __instance.maps.Length);
         __instance.requestedMapID = num;
-        __instance.currentMap = PhotonNetwork.Instantiate("Maps/" + __instance.maps[num].name, Vector3.zero, Quaternion.identity, 0, null);
-        GameObject gameObject = __instance.currentMap;
+        string mapName = __instance.maps[num].name;
 
-        if (gameObject.name.ToLower().Contains("oldmap"))
+        __instance.currentMap = PhotonNetwork.Instantiate("Maps/" + mapName, Vector3.zero, Quaternion.identity, 0, null);
+
+        if (mapName.ToLower().Contains("oldmap"))
         {
-            Debug.Log("Scaling up old map!");
-            gameObject.transform.localScale = new Vector3(100, 100, 100);
-            gameObject.transform.position = new Vector3(0, -20, 0);
+            __instance.currentMap.transform.position = new Vector3(0, -25, 0);
 
-            Debug.Log("Extracting textures from map0...");
-            List<Material> defaultMats = new List<Material>();
-            List<Transform> children = __instance.maps[0].GetComponentsInChildren<Transform>().ToList();
-            children.AddRange(__instance.maps[1].GetComponentsInChildren<Transform>().ToList());
-            children.AddRange(__instance.maps[2].GetComponentsInChildren<Transform>().ToList());
-            children.AddRange(__instance.maps[3].GetComponentsInChildren<Transform>().ToList());
-            children.AddRange(__instance.maps[4].GetComponentsInChildren<Transform>().ToList());
-
-            foreach (Transform child in children)
+            RaiseEventOptions raiseEventOptions = new RaiseEventOptions
             {
-                if (child.GetComponent<MeshRenderer>() != null)
+                Receivers = ReceiverGroup.Others,
+                CachingOption = EventCaching.AddToRoomCache
+            };
+            SendOptions sendOptions = new SendOptions { Reliability = true };
+            foreach (Transform child in __instance.currentMap.GetComponentsInChildren<Transform>())
+            {
+                if (child.name.Contains("Bomb"))
                 {
-                    Debug.Log("Object with meshrenderer found! Attempting to extract material...");
-                    if (defaultMats.FindIndex(x => child.GetComponent<MeshRenderer>().sharedMaterial.name.Contains(x.name)) == -1)
-                    {
-                        defaultMats.Add(child.GetComponent<MeshRenderer>().sharedMaterial);
-                        Debug.Log("Extracted texture " + child.GetComponent<MeshRenderer>().sharedMaterial.name);
-                    }
-                    else
-                    {
-                        Debug.Log("Mats list already contains texture. Skipping...");
-                    }
-                }
-                else
-                {
-                    Debug.Log("Skipping object with no meshrenderer...");
-                }
-            }
-            Debug.Log("Textures extracted! List is as follows...");
-            foreach (Material mat in defaultMats)
-            {
-                Debug.Log(mat.name);
-            }
-            Debug.Log("Loading textures...");
-            foreach (Transform child in gameObject.GetComponentsInChildren<Transform>(true))
-            {
-                if (child.GetComponent<MeshRenderer>() != null)
-                {
-                    int findIdx = defaultMats.FindIndex(x => child.GetComponent<MeshRenderer>().sharedMaterial.name.Contains(x.name));
-                    if (findIdx != -1)
-                    {
-                        child.GetComponent<MeshRenderer>().sharedMaterial = defaultMats[findIdx];
-                        Debug.Log("Replaced texture with " + defaultMats[findIdx].name);
-                    }
-                    else
-                    {
-                        Debug.Log($"Unable to find material {child.GetComponent<MeshRenderer>().sharedMaterial.name} in main list");
-                    }
-                }
-
-                if (child.name.Contains("Spawn"))
-                {
-
-                    child.gameObject.AddComponent<MapSpawnPosition>();
-                    Debug.Log($"Successfully converted {child.name} to a functional MapSpawnPosition!");
-
-                }
-            }
-            foreach (MeshCollider cl in gameObject.GetComponentsInChildren<MeshCollider>())
-            {
-                GameObject current = cl.gameObject;
-                UnityEngine.Object.Destroy(cl);
-                current.AddComponent<BoxCollider>();
-            }
-            Physics.SyncTransforms();
-            foreach (Rigidbody rb in gameObject.GetComponentsInChildren<Rigidbody>())
-            {
-                rb.solverIterations = 20;
-                rb.solverVelocityIterations = 20;
-                rb.mass = 700;
-                rb.ResetCenterOfMass();
-                rb.ResetInertiaTensor();
-                rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
-            }
-            Physics.SyncTransforms();
-
-            foreach (Transform child in gameObject.GetComponentsInChildren<Transform>(true))
-            {
-                GameObject current = child.gameObject;
-                Rigidbody rb = current.GetComponent<Rigidbody>();
-                if (current.name.Contains("Bomb"))
-                {
-                    Vector3 position = current.transform.position;
+                    Vector3 position = child.transform.position;
 
                     PhotonNetwork.Instantiate("Misc/" + "MO_A_Bomb", position, Quaternion.identity);
                 }
-                else if (rb != null)
-                {
-                    Effectable effectable = current.AddComponent<Effectable>();
-                    Effectable_MapObject effectableMap = current.AddComponent<Effectable_MapObject>();
-
-
-                }
             }
+
+            int mapViewID = __instance.currentMap.GetComponent<PhotonView>().ViewID;
+            PhotonNetwork.RaiseEvent(MAP_MOVE_EVENT_CODE, mapViewID, raiseEventOptions, sendOptions);
         }
 
-
-        //Time.timeScale = 0f;
-        Debug.Log($"Successfully loaded map: {gameObject.name} (Index: {num})");
+        Debug.Log($"Successfully loaded map: {mapName} (Index: {num})");
         return false;
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == MAP_MOVE_EVENT_CODE)
+        {
+            int targetViewID = (int)photonEvent.CustomData;
+            PhotonView targetView = PhotonView.Find(targetViewID);
+
+            if (targetView != null)
+            {
+                targetView.gameObject.transform.position = new Vector3(0, -23, 0);
+                Debug.Log("Successfully moved map on client via Network Event.");
+            }
+        }
     }
 }
 public static class Utils
